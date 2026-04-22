4119aac5be1a5e82c2228150d563fecdbde19331
Ayush gopal <ayushgopal17@gmail.com>
2026-04-22 20:19:30 +0530

    intilized backend

--- index.js
+++ index.js
@@ -1,46 +1,86 @@
 const express=require ("express");
 
-const users=[{
+let USERS_ID =1;
+let ORGANIZATIONS_ID=1;
+let BOARDS_ID=1;
+let ISSUES_ID=1;
+
+const USERS=[{
     id:1,
     username: "Ayush",
     password: "123123"
-},
-{
-    id:2,
-    username: "Aman",
-    password: "123456"
 }]
-const boards=[{
+const ORGANIZATIONS=[{
     id:1,
     title: "100xdevs",
     description: "Learning coding platform",
     admin: 1,
     member: [2]
-},{
-    id:2,
-    title: "Ayush's Org",
-    description: "Learning",
-    admin: 1,
-    memeber: []
 }];
-const organisations=[{
+const BOARDS=[{
     id:1,
     title: "100xschool website (frontend)",
     organisationId:1
 }];
 
-const issues=[{
+const ISSUES=[{
     id:1,
     title: "Add dark mode",
-    boardId: 1
-},{
-    id:2,
-    title: "Allow admin to create more courses",
-    boardId:2
+    boardId: 1,
+    state: "IN_PROGRESS"
 }];
-const issues=[];
-
 
 const app= express()
+
+app.post('/singup',(req,res)=>{
+
+    const username= req.body.username;
+    const password= req.body.password;
+
+    const userExist= USERS.find(u=> u.username === username);
+    if(userExist){
+        res.status(411).send({
+            message: "User With this Username Already Exists"
+        })
+    }
+})
+
+app.post('/singin',(req,res)=>{
+
+})
+
+app.post('/organizations',(req,res)=>{
+
+})
+app.post('/add-member-to-org',(req,res)=>{
+
+})
+
+app.post('/board',(req,res)=>{
+
+})
+
+app.post('/issue',(req,res)=>{
+
+})
+
+app.get('/board',(req,res)=>{
+
+})
+app.get('/issues',(req,res)=>{
+    
+})
+app.get('/members',(req,res)=>{
+    
+})
+
+app.put('/issues',(req,res)=>{
+
+})
+
+app.delete('/members',(req,res)=>{
+
+})
+
 
 app.listen(3000);
